fb9fc226247a33d0ddba98c6acb65fa2da20d505
Hadgebury <hadgebury@yahoo.co.uk>
2025-05-04 12:20:00 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'Hadgebury'
 description 'Prevents despawning off vehicles used by players'
-version '1.0.3'
+version '1.0.0'
 
 client_script 'client.lua'
 server_script 'server.lua'
